4dc2299a11e702b1b606a4accb70ef2448fb4270
Pawan Rama Mali <prm@outlook.in>
2021-09-05 00:59:16 +0530

    added users and job class

--- backend/core/config.py
+++ backend/core/config.py
@@ -6,7 +6,7 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 class Settings:
-    PROJECT_NAME:str = "Job Board"
+    PROJECT_NAME:str = "Jobstack"
     PROJECT_VERSION: str = "1.0.0"
 
     POSTGRES_USER : str = os.getenv("POSTGRES_USER")
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
-from core.config import Settings
+from fastapi.staticfiles import StaticFiles
+from core.config import settings
+#from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine   #new
+from db.base_class import Base  #new
 
 
-app = FastAPI(title = Settings.PROJECT_TITLE, version=Settings.PROJECT_VERION)
+def include_router(app):
+	app.include_router(general_pages_router)
 
-@app.get("/")
-def hello_api():
-        return {"detail":"hello world"}
+
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def create_tables():           #new
+	Base.metadata.create_all(bind=engine)
+
+	
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	#include_router(app)
+	#configure_static(app)
+	create_tables()       #new
+	return app
+
+app = start_application()
